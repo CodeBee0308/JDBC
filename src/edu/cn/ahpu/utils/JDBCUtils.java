0b6d86fbd5f9222877610622077fbf2c9d77cf8a
CodeBee <CodeBee9527@gmail.com>
2020-10-14 12:22:52 +0800

    清除部分无用文件

--- src/edu/cn/ahpu/utils/JDBCUtils.java
+++ src/edu/cn/ahpu/utils/JDBCUtils.java
@@ -8,31 +8,49 @@ import java.sql.Statement;
 import java.sql.SQLException;
 import java.util.Properties;
 
+/**
+ * 
+ * @Description JDBC工具类
+ * @author CodeBee Email:CodeBee@gmial.com
+ * @version 
+ * @date 2020年10月13日下午4:33:03
+ *
+ */
 public class JDBCUtils {
 
 	/**
 	 * 
-	 * @return
+	 * @Description 获取数据库连接
+	 * @author CodeBee
+	 * @date 2020年10月13日下午4:33:10
+	 * @return 数据库连接对象
 	 * @throws Exception
 	 */
 	public static Connection getConnection() throws Exception {
+		// 3.1、用于为读取与数据库相关的配置文件信息做准备
 		InputStream is = ClassLoader.getSystemClassLoader().getResourceAsStream("jdbc.properties");
 		Properties pros = new Properties();
 		pros.load(is);
-		
+		// 3.2、读取配置文件中的与数据库相关的信息，并将读取的参数传递到DriverManger的构造器中
 		String user = pros.getProperty("user");
 		String password = pros.getProperty("password");
-		String driverClass = pros.getProperty("driverClass");
 		String url = pros.getProperty("url");
+			// 为注册数据库做准备
+		String driverClass = pros.getProperty("driverClass");
 		
+		// 1、注册数据驱动
 		Class.forName(driverClass);
 		
+		// 2、通过DriverManager来获取连接
 		Connection conn = DriverManager.getConnection(url, user, password);
 		return conn ;
 	}
 	
 	/**
 	 * 
+	 * @Description 数据库资源的关闭
+	 * @author CodeBee
+	 * @date 2020年10月13日下午4:34:35
 	 * @param conn
 	 * @param ps
 	 */
@@ -53,7 +71,15 @@ public class JDBCUtils {
 			}
 		}
 	}
-	
+	/**
+	 * 
+	 * @Description 数据库资源的关闭
+	 * @author CodeBee
+	 * @date 2020年10月13日下午4:35:22
+	 * @param conn
+	 * @param ps
+	 * @param rs
+	 */
 	public static void closeResource(Connection conn,Statement ps,ResultSet rs) {
 		// 资源的关闭
 		if (ps != null) {
--- src/edu/cn/ahpu1/Connection/ConnectionTest.java
+++ src/edu/cn/ahpu1/Connection/ConnectionTest.java
@@ -10,22 +10,26 @@ import org.junit.Test;
 
 /**
  * 
-* @Description  获取数据库连接
-* @author CodeBee Email:CodeBee@gmial.com
-* @version
-* @date 2020年10月12日下午9:53:09
-*
+ * @Description 获取数据库连接的五种方式
+ * @author CodeBee Email:CodeBee@gmial.com
+ * @version
+ * @date 2020年10月13日下午4:54:22
+ *
  */
-
 public class ConnectionTest {
 	
 	/**
-	 *
+	 * 
+	 * @Description 方式一
+	 * @author CodeBee
+	 * @date 2020年10月14日上午10:56:16
 	 * @throws Exception
 	 */
 	@Test
 	public void testConnection01() throws Exception{
 		//1、 获取Java原生API中Driver接口的实现类对象
+		// com.mysql.cj.jdbc.Driver()为java.sql.Driver接口的实现类
+		// 子类对象指向父类的引用，多态性，所以此时driver在编译期调用的父类中的抽象方法，到运行时再调用子类中重写的方法 ；
 		Driver driver = new com.mysql.cj.jdbc.Driver();
 		
 		// url:http://localhost:8080/gmall/keyboard.jpg
@@ -38,16 +42,24 @@ public class ConnectionTest {
 		Properties info = new Properties();
 		info.setProperty("user", "root") ;
 		info.setProperty("password", "password");
-		//2、调用方法
+		//2、编译器调用的是父接口中的抽象方法，运行时调用的是实现类中重写的方法 ；
 		Connection conn = driver.connect(url, info);
 		System.out.println(conn);
 		
 	}
 	
+	/**
+	 * 
+	 * @Description 方式二：解决方式一中使用了非Java原生的API(com.mysql.cj.jdbc.Driver())
+	 * @author CodeBee
+	 * @date 2020年10月14日上午10:56:32
+	 * @throws Exception
+	 */
 	@Test
 	public void testConnection02() throws  Exception {
-		// 获取Driver实现类对象：使用反射
+		// 通过反射的方式获取实现类的父类
 		Class cla = Class.forName("com.mysql.cj.jdbc.Driver");
+		// 利用获取到的父类来实例化子类对象
 		Driver driver = (Driver) cla.newInstance();
 		
 		
